d340d35ac214b8692c7437a977a62ad6eb6f7b62
Mikita Navicki <bulbanik91@gmail.com>
2022-01-02 20:02:21 +0300

    Прохождение pytest, осталась 1 ошибка

--- homework.py
+++ homework.py
@@ -37,21 +37,25 @@ logger.addHandler(logging.StreamHandler())
 
 class EmptyListOrDictionaryError(Exception):
     """Пустой список или словарь."""
+
     pass
 
 
 class ResponseStatusCodeError(Exception):
     """Некорректный статус ответа сервера."""
+
     pass
 
 
 class RequestExceptionError(Exception):
     """Некорректный запрос."""
+
     pass
 
 
 class IndefinеStatusError(Exception):
     """Неопределённый статус ответа."""
+
     pass
 
 
